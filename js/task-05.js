b3ae7e1efa063f5be751f47c72973dbbc675ee39
Tetiana2023 <drofaj18@gmail.com>
2022-12-16 19:24:14 +0200

    task -05

---
+++ js/task-05.js
@@ -0,0 +1,18 @@
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', onInputName)
+
+function onInputName (event)  {
+    spanEl.textContent = event.currentTarget.value;
+    
+    if ( !inputEl.value ) {
+      spanEl.textContent = "Ananimus";
+    
+   }
+     
+
+};
+
+//console.log(inputEl); 
+//console.log(spanEl);
