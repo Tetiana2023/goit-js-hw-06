044d3d38477f292abbea185f59048fbc0d40c390
Tetiana2023 <drofaj18@gmail.com>
2022-12-18 23:36:47 +0200

    task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick( event){
+ 
+  
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  console.log(bodyEl)
+  spanEl.textContent = bodyEl.style.backgroundColor;
+
+}
+
+
+
+
+
+
+console.log(buttonEl);
+console.log(spanEl);
