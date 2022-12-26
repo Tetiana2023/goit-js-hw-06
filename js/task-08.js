5aec82211a944c8d464e0e6e9b3945fbfa9b0efe
Tetiana2023 <drofaj18@gmail.com>
2022-12-26 20:56:24 +0200

    task -08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", onBtnSubmit)
+
+function onBtnSubmit (event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+    
+}
+
+console.log(`Email: ${email.value}, Password: ${password.value}`);
+event.currentTarget.reset();
+}
+
+
+
 
