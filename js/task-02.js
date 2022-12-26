9759d9f2e677fe3ede634d944f7c3dff4a7acf31
Tetiana2023 <drofaj18@gmail.com>
2022-12-26 20:31:29 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -7,18 +7,31 @@ const ingredients = [
   'Condiments',
 ];
 
-const ulIgreientsEl = document.querySelector('#ingredients')
+//const ulIngredientsEl = document.querySelector('#ingredients')
 //console.log(ulIgreientsEl);
 
- for (let ingredient of ingredients) {
-const liEl = document.createElement('li');
+// for (let ingredient of ingredients) {
+//const liEl = document.createElement('li');
 
- liEl.textContent = ingredient;
- liEl.classList.add('item');
+ //liEl.textContent = ingredient;
+ //liEl.classList.add('item');
  
- ulIgreientsEl.append(liEl);
- }
- console.log(ulIgreientsEl);
+ //ulIgreientsEl.append(liEl);
+ //}
+ //console.log(ulIgreientsEl);
+
+// ========================================================
+const ulIngredientsEl = document.querySelector('#ingredients')
+
+const list = ingredients.map((ingredient) => {
+    let liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+   return liEl;
+  });
+   ulIngredientsEl.append(...list);
+
+//========================================================
 
  //for (let ingredient of ingredients) {
   //  let liEl = document.createElement("li");
