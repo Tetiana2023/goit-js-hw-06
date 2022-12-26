5abe321643c14b0650686d8b398abe18fd283652
Tetiana2023 <drofaj18@gmail.com>
2022-12-26 20:34:45 +0200

    task -06

--- js/task-06.js
+++ js/task-06.js
@@ -9,7 +9,6 @@ if (event.currentTarget.value.length === +inputEl.dataset.length) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
     
-
 } 
 else {
     inputEl.classList.add('invalid');
